3b9e6545c2a4f40d52d7fae00f923dcf1ba2b012
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2024-04-12 17:54:50 -0300

    Atualização

--- lib/ui/life_cycle_of_widget.dart
+++ lib/ui/life_cycle_of_widget.dart
@@ -6,19 +6,14 @@ import 'package:flutter/scheduler.dart';
 import 'package:workmanager/workmanager.dart';
 
 class LifeCycleWidget1 extends StatefulWidget {
-  const LifeCycleWidget1({Key? key}) : super(key: key);
+  const LifeCycleWidget1({super.key});
 
   @override
   State<LifeCycleWidget1> createState() => _LifeCycleWidget1State();
 }
 
 class _LifeCycleWidget1State extends State<LifeCycleWidget1> {
-
   String randomName = 'Krishna';
-  @override
-  void initState() {
-
-  }
 
   @override
   void didChangeDependencies() {
@@ -46,15 +41,18 @@ class _LifeCycleWidget1State extends State<LifeCycleWidget1> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          ElevatedButton(onPressed: updateRandomName, child: Text('pass random button')),
-          ElevatedButton(onPressed: isolate, child: Text('isolates')),
-          ElevatedButton(onPressed: workManger, child: Text('work manger')),
-           LifeCycleChildWidget(randomName)
+          ElevatedButton(
+              onPressed: updateRandomName,
+              child: const Text('pass random button')),
+          ElevatedButton(onPressed: isolate, child: const Text('isolates')),
+          ElevatedButton(
+              onPressed: workManger, child: const Text('work manger')),
+          LifeCycleChildWidget(randomName)
         ],
       ),
     );
@@ -62,25 +60,23 @@ class _LifeCycleWidget1State extends State<LifeCycleWidget1> {
 
   void isolate() async {
     var res = await compute(runTimer, 60);
-    print(res);
+    if (kDebugMode) {
+      print(res);
+    }
   }
 
   void workManger() {
     var si = 'be.tramckrijte.workmanagerExample.simpleTask';
-    Workmanager().registerOneOffTask(
-        si,
-        si,
-        initialDelay: Duration(seconds: 10),
-        inputData: <String, dynamic>{
-          'int': 1 }
-    );
+    Workmanager().registerOneOffTask(si, si,
+        initialDelay: const Duration(seconds: 10),
+        inputData: <String, dynamic>{'int': 1});
   }
 
-  static Future<int>  runTimer(int a) async {
+  static Future<int> runTimer(int a) async {
     // SharedPreferences prefs = await SharedPreferences.getInstance();
     int i = 0;
-    while(i < a) {
-      await Future.delayed(Duration(seconds: 1));
+    while (i < a) {
+      await Future.delayed(const Duration(seconds: 1));
       // prefs.setInt('getTime', i);
       i++;
     }
@@ -89,35 +85,57 @@ class _LifeCycleWidget1State extends State<LifeCycleWidget1> {
 
   void updateRandomName() {
     setState(() {
-     var r = Random();
-     randomName = String.fromCharCodes(List.generate(10, (index) => r.nextInt(33) + 89));
+      var r = Random();
+      randomName = String.fromCharCodes(
+          List.generate(10, (index) => r.nextInt(33) + 89));
     });
   }
 }
 
 class LifeCycleChildWidget extends StatefulWidget {
   final String randomName;
-  const LifeCycleChildWidget(this.randomName, {Key? key}) : super(key: key);
+  const LifeCycleChildWidget(this.randomName, {super.key});
 
   @override
   State<LifeCycleChildWidget> createState() => _LifeCycleChildWidgetState();
 }
 
-class _LifeCycleChildWidgetState extends State<LifeCycleChildWidget> with SingleTickerProviderStateMixin {
-
+class _LifeCycleChildWidgetState extends State<LifeCycleChildWidget>
+    with SingleTickerProviderStateMixin {
   late AnimationController ctrl;
+ 
   @override
   void initState() {
-    print('idle phase 1 : ${SchedulerBinding.instance.schedulerPhase}'); /// SchedulerPhase.idle
+    if (kDebugMode) {
+      print('idle phase 1 : ${SchedulerBinding.instance.schedulerPhase}');
+    }
 
-    ctrl = AnimationController(vsync: this, duration: Duration(seconds: 1))..addListener(() {
-      print('Animation started phase : ${SchedulerBinding.instance.schedulerPhase}'); /// transient callbacks
-    })..forward();
+    /// SchedulerPhase.idle
+    ctrl =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1))
+          ..addListener(() {
+            if (kDebugMode) {
+              print(
+                  'Animation started phase : ${SchedulerBinding.instance.schedulerPhase}');
+            }
+
+            /// transient callbacks
+          })
+          ..forward();
+
     /// this will be executed one frame is constructed only.
-    Future.delayed(Duration(seconds: 2), () {
-      print('shceduler phase 2 : ${SchedulerBinding.instance.schedulerPhase}');
-      someAsyncronusTask();
-    },); ///
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        if (kDebugMode) {
+          print(
+              'shceduler phase 2 : ${SchedulerBinding.instance.schedulerPhase}');
+        }
+        someAsyncronusTask();
+      },
+    );
+
+    ///
     super.initState();
   }
 
@@ -128,7 +146,9 @@ class _LifeCycleChildWidgetState extends State<LifeCycleChildWidget> with Single
 
   @override
   void didUpdateWidget(covariant LifeCycleChildWidget oldWidget) {
-    var a = List.generate(1000000, (index) => 1000000/2).toList();  /// this line of code creates junk
+    var a = List.generate(1000000, (index) => 1000000 / 2).toList();
+
+    /// this line of code creates junk
     super.didUpdateWidget(oldWidget);
   }
 
@@ -146,14 +166,14 @@ class _LifeCycleChildWidgetState extends State<LifeCycleChildWidget> with Single
     );
   }
 
-
   String performSomeArthmaticOperations() {
     return widget.randomName;
   }
 
   Future<void> someAsyncronusTask() async {
-    print('asyncronus method : ${SchedulerBinding.instance.schedulerPhase}');
+    if (kDebugMode) {
+      print('asyncronus method : ${SchedulerBinding.instance.schedulerPhase}');
+    }
     await Future.delayed(const Duration(seconds: 1));
   }
 }
-
